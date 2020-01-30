4e3c3af41bc59f6156630b25a1e81d46d91211dc
evr01 <erosa26@gmail.com>
2020-01-30 09:14:50 -0500

    fonts roboto import

---
+++ pages/projects/data-science/chart-1/D3Chart.js
@@ -0,0 +1,15 @@
+import * as d3 from 'd3';
+
+const data = [20, 10, 12, 16, 25]
+
+export default class D3Chart {
+
+    constructor(element) {
+        const svg = d3.select(element).append('svg').attr('width', 500).attr('height', 500);
+
+        const rects = svg.selectAll('rect').data(data);
+
+        rects.enter().append('rect').attr('x', (d, i) => i * 100).attr('y', 50).attr('width', 50).attr('height', d => d).attr('fill', 'grey');
+    }
+
+}
--- pages/projects/data-science/chart-1/index.js
+++ pages/projects/data-science/chart-1/index.js
@@ -1,58 +1,15 @@
 import React from 'react';
 import Layout from '../../../../components/Layout';
+import ChartWrapper from './ChartWrapper';
 import axios from 'axios';
-import {
-    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
-} from 'recharts';
+
 
 class Chart extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: []
-        };
-        this.getData = this.getData.bind(this);
-    }
-    getData() {
-        const url = `https://evro-prod-backend.herokuapp.com/api/evro/data/wine/`;
-        axios
-            .get(url)
-            .then((res) => {
-                this.setState({
-                    data: res.data,
-
-                });
-                console.log(res.data);
-                console.log(data.wine_id);
-            })
-            .catch((error) => this.setState({ error }));
-
-
-    }
-
-    componentDidMount() {
-        this.getData();
-    }
-
     render() {
         return (
             <Layout>
                 <h1>hi</h1>
-                <BarChart
-                    width={500}
-                    height={300}
-                    data={this.props.data}
-                    margin={{
-                        top: 5, right: 30, left: 20, bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="region_1" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="region_1" fill="#8884d8" />
-                </BarChart>
+                <ChartWrapper />
             </Layout>
         )
     }
